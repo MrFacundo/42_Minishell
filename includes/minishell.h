7f7b6c85d1a7407bcd474a046bba205899c41300
Facundo Troitero <ftroiter@c1r8s1.42lisboa.com>
2023-10-20 21:49:07 +0100

    first commit

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ftroiter <ftroiter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 17:21:40 by ftroiter          #+#    #+#             */
-/*   Updated: 2023/10/20 17:35:52 by ftroiter         ###   ########.fr       */
+/*   Updated: 2023/10/20 21:11:11 by ftroiter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define MINISHELL
 
 # include <stdio.h> // printf
+# include <readline/readline.h>
+# include <readline/history.h>
 # include "../libft/libft.h"
 
 #endif
--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,16 +6,26 @@
 /*   By: ftroiter <ftroiter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 17:20:06 by ftroiter          #+#    #+#             */
-/*   Updated: 2023/10/20 17:36:29 by ftroiter         ###   ########.fr       */
+/*   Updated: 2023/10/20 21:48:15 by ftroiter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+int getcmd(char *buf)
+{
+	rl_initialize();
+	buf = readline("ms$ ");
+	if(buf[0] == 0) // EOF
+    	return -1;
+	return 0;
+}
+
 int	main()
 {
-	char *str = "123";
+  char *buf;
 
-	printf("%d\n", ft_atoi(str));
-	return (0);
+  while(getcmd(buf) >= 0)
+  	printf("buf: %s\n", buf);
+  return (0);
 }
