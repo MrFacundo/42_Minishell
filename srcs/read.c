4da860ff27d70a67d4224fe40e1e95f9a9d6b0b0
A.Mak <mak_alexandre@hotmail.com>
2023-12-10 18:39:58 +0000

    heredoc : corrected leaks

--- srcs/read.c
+++ srcs/read.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   read.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: facu <facu@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: amak <amak@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 18:07:29 by facu              #+#    #+#             */
-/*   Updated: 2023/12/04 22:36:55 by facu             ###   ########.fr       */
+/*   Updated: 2023/12/10 18:34:38 by amak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,7 @@ int	read_heredoc(char *delimiter)
             break;
         write(fd[1], line, ft_strlen(line));
         write(fd[1], "\n", 1);
+		free(line);
 	}
 	free(line);
 	close(fd[1]);
